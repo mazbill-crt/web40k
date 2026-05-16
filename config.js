3f625aa9683bb0f5139d29bc40e07f98bc3a9e17
mazbill-crt <billyrosyad954@gmail.com>
2026-05-16 22:55:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+//credit By Izal
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "https://jazzprivat.ymzprivat.biz.id", // Ubah jadi Domain panel Mu !!!
+  port: "1973" // Ubah Jadi Port Panel Mu !!!
+};
