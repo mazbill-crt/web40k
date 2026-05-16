d9eda5bade13f9779e58a8f4f725391f665f1fe5
mazbill-crt <billyrosyad954@gmail.com>
2026-05-16 23:14:39 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,7 @@
+//credit By Izal
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "sftp://tokopanel151-gondrongnodes.panelshop.biz.id", // Ubah jadi Domain panel Mu !!!
+  port: "2022" // Ubah Jadi Port Panel Mu !!!
+};
