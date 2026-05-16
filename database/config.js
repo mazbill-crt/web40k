039c2d09c6eb04fa028a6800c61f5695826b2aae
mazbill-crt <billyrosyad954@gmail.com>
2026-05-16 23:14:14 +0700

    Delete database/config.js

--- database/config.js
+++
@@ -1,7 +0,0 @@
-//credit By Izal
-//no hapus credit ketauan hapus hitam//
-
-module.exports = {
-  domain: "https://jazzprivat.ymzprivat.biz.id", // Ubah jadi Domain panel Mu !!!
-  port: "1973" // Ubah Jadi Port Panel Mu !!!
-};
